1bc670437736c138a8c122e4fab42e85f67b6866
Harsh-gamma007 <99949028+Harsh-gamma007@users.noreply.github.com>
2022-04-19 18:11:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './Header'
-import AddUser from './AddUser'
-import EditUser from './EditUser'
-import UserList from './UserList'
-import Notfound from './NotFound'
+
+const Header = React.lazy(() => import('./Header'))
+const AddUser = React.lazy(() => import('./AddUser'))
+const UserList = React.lazy(() => import('./UserList'))
+const Notfound = React.lazy(() => import('./NotFound'))
+const EditUser = React.lazy(() => import('./EditUser'))
 
 function App() {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route exact path="/" element={<UserList />} />
-        <Route exact path="/addUser" element={<AddUser />} />
-        <Route exact path="/user/editUser/:id" element={<EditUser />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route exact path="/" element={<UserList />} />
+          <Route exact path="/addUser" element={<AddUser />} />
+
+          <Route exact path="/user/editUser/:id" element={<EditUser />} />
+
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
