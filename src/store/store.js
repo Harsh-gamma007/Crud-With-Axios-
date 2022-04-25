3c7ce9d3338d738093b52955daad7ac992b88fbc
Harsh-gamma007 <99949028+Harsh-gamma007@users.noreply.github.com>
2022-04-25 18:21:05 +0530

    Delete src directory

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../components/UserSlice'
-
-export default configureStore({
-  reducer: {
-    users: userReducer,
-  },
-})
